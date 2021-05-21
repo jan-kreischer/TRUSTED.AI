019200dfc22ecf6223a19ad02a67e9c69b92f416
Jan Bauer <jan.bauer@kreischers.com>
2021-05-21 12:08:23 +0200

    Added master project description

---
+++ webapp/muriel/frontend/src/main.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import {
+  MdTabs,
+  MdCard,
+  MdButton,
+  MdIcon,
+  MdDivider,
+  MdSnackbar,
+  MdField,
+  MdList,
+  MdDialog,
+  MdEmptyState,
+  MdSpeedDial,
+  MdTooltip,
+  MdProgress,
+  MdSwitch,
+  MdCheckbox,
+  MdBadge,
+  MdChips } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdTabs)
+Vue.use(MdCard)
+Vue.use(MdButton)
+Vue.use(MdIcon)
+Vue.use(MdDivider)
+Vue.use(MdSnackbar)
+Vue.use(MdField)
+Vue.use(MdList)
+Vue.use(MdDialog)
+Vue.use(MdTooltip)
+Vue.use(MdSpeedDial)
+Vue.use(MdEmptyState)
+Vue.use(MdProgress)
+Vue.use(MdChips)
+Vue.use(MdCheckbox)
+Vue.use(MdBadge)
+Vue.use(MdSwitch)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
