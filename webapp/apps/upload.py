2b3d3ec8e86d85989c72293bc072c097abed3262
Jan Bauer <jan.kr.bauer@gmail.com>
2021-08-15 19:17:01 +0200

    Fixed import errors

--- webapp/apps/upload.py
+++ webapp/apps/upload.py
@@ -4,7 +4,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_table
+import pandas as pd
+import json
+import pickle
 import os
+import io
+import base64
 from app import app
 
 # === CALLBACKS ===
