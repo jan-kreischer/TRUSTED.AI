571b3fb954b693aae848b6232a7f2131dc3706ae
Jan Bauer <jan.kr.bauer@gmail.com>
2021-08-17 15:19:27 +0200

    Renamed trustscore-button to upload_button

--- webapp/sites/factsheet.py
+++ webapp/sites/factsheet.py
@@ -13,17 +13,55 @@ import base64
 from app import app
 from config import SCENARIOS_FOLDER_PATH
 
+
+
 layout = dbc.Container([
     dbc.Col([], className="mb-4"),
     
     dbc.Col([
         html.Div([
-            html.H3("2. Solution"),
-        ], 
-        className="text-center"
+            html.H1("Factsheet"),
+            html.H3("Model Name"),
+            dcc.Input(id="model_name", type="text", placeholder="", value="", debounce=True, style={'width': '100%', 'textAlign': 'center'}), 
+            
+            #--- Purpose ---
+            html.H3("Purpose"),
+            dcc.Textarea(
+                id='purpose_description',
+                value='',
+                style={'width': '100%', 'height': 150},
+            ),
+            
+            #--- Domain ---
+            html.H3("Domain"),
+            dcc.Textarea(
+                id='domain_description',
+                value='',
+                style={'width': '100%', 'height': 150},
+            ),
+            
+            #--- Domain ---
+            html.H3("Training Data"),
+            dcc.Textarea(
+                id='training_data_description',
+                value='',
+                style={'width': '100%', 'height': 150},
+            ),
+            
+            html.H3("Y Column Name"),
+            dcc.Input(id="y_column_name", type="text", placeholder="", value="", debounce=True, style={'width': '100%'}),
+            
+            #--- Domain ---
+            html.H3("Contact Information"),
+            dcc.Textarea(
+                id='contact_information',
+                value='',
+                style={'width': '100%', 'height': 150},
+            ),
+        ]
         ),
     ], 
-    className="mb-4"
+    className=""
     ),
     
     html.Div(dbc.Button("Download",  id='download_factsheet', color="primary", className="mt-3"), className="text-center"),
--- webapp/sites/upload.py
+++ webapp/sites/upload.py
@@ -63,7 +63,7 @@ def model_preview(content, name):
 
 # --- Validation Callbacks --- #
 @app.callback(Output('problem_set_alert', 'children'),
-              [Input('trustscore-button', 'n_clicks'),
+              [Input('upload_button', 'n_clicks'),
                Input('problem_set', 'value'),
                ], prevent_initial_call=True)
 def validate_problem_set(n_clicks, problem_set):
@@ -74,7 +74,7 @@ def validate_problem_set(n_clicks, problem_set):
             return html.H6("No problem set was selected", style={"color":"Red"})
   
 @app.callback(Output('model_name_alert', 'children'),
-              [Input('trustscore-button', 'n_clicks'),
+              [Input('upload_button', 'n_clicks'),
                Input('problem_set', 'value'),
                Input('model_name', 'value'),
                ], prevent_initial_call=True)
@@ -91,7 +91,7 @@ def validate_model_name(n_clicks, problem_set, model_name):
                 return None
             
 @app.callback(Output('training_data_alert', 'children'),
-               [Input('trustscore-button', 'n_clicks'),
+               [Input('upload_button', 'n_clicks'),
                 Input('training_data_upload', 'contents')], prevent_initial_call=True
                 )
 def validate_training_data(n_clicks, training_data):
@@ -102,7 +102,7 @@ def validate_training_data(n_clicks, training_data):
             return None
         
 @app.callback(Output('test_data_alert', 'children'),
-               [Input('trustscore-button', 'n_clicks'),
+               [Input('upload_button', 'n_clicks'),
                 Input('test_data_upload', 'contents')], prevent_initial_call=True
                 )
 def validate_test_data(n_clicks, test_data):
@@ -114,7 +114,7 @@ def validate_test_data(n_clicks, test_data):
 
 
 @app.callback(Output('factsheet_alert', 'children'),
-               [Input('trustscore-button', 'n_clicks'),
+               [Input('upload_button', 'n_clicks'),
                 Input('factsheet_upload', 'filename'),
                 Input('factsheet_upload', 'contents')
                ], prevent_initial_call=True)
@@ -129,7 +129,7 @@ def validate_factsheet(n_clicks, factsheet_name, factsheet_content):
             return None
         
 @app.callback(Output('model_alert', 'children'),
-               [Input('trustscore-button', 'n_clicks'),
+               [Input('upload_button', 'n_clicks'),
                 Input('model_upload', 'contents')],
                 prevent_initial_call=True)
 def validate_model(n_clicks, model):
@@ -140,7 +140,7 @@ def validate_model(n_clicks, model):
             return None
         
 @app.callback(Output('uploaded_solution_set_path', 'data'),
-    [Input('trustscore-button', 'n_clicks'),
+    [Input('upload_button', 'n_clicks'),
     State('problem_set', 'value'),
     State('model_name', 'value')], prevent_initial_call=True)
 def save_new_model_name(n_clicks, problem_set_path, model_name):
@@ -153,7 +153,7 @@ def save_new_model_name(n_clicks, problem_set_path, model_name):
 
 @app.callback(Output('upload_alert', 'children'),
               [
-               Input('trustscore-button', 'n_clicks'),
+               Input('upload_button', 'n_clicks'),
                State('problem_set', 'value'),
                State('model_name', 'value'),
                State('training_data_upload', 'contents'),
@@ -209,7 +209,7 @@ def upload_data(
             return dcc.Location(pathname="/analyze", id="someid_doesnt_matter")
             return html.H5("Upload Successful", className="text-center")
 
-modals = ["problem_set", "solution_set", "training_data", "test_data", "factsheet", "model"]
+modals = ["problem_set", "solution_set", "training_data", "test_data", "target_column" ,"factsheet", "model"]
 for m in modals:
     @app.callback(
         Output("{}_info_modal".format(m), "is_open"),
@@ -318,7 +318,7 @@ layout = dbc.Container([
         html.Div([
             create_info_modal("problem_set", "Scenario", "All different solutions found should belong to the same scenario"),
             html.Div(id="problem_set_alert"),
-            html.H3("1. Scenario"),
+            html.H3(["1. Scenario", html.Sup("*")]),
             html.H5("Please select the scenario your solution belongs to.")
         ], className="text-center"),
         dcc.Dropdown(
@@ -334,7 +334,7 @@ layout = dbc.Container([
         html.Div([
             create_info_modal("solution_set", "Solution", "One specifically trained model including its training-, test data and factsheet can be seen as a solution set. Your solution set will be saved under the name you entered here."),
             html.Div(id="model_name_alert"),
-            html.H3("2. Solution"),
+            html.H3(["2. Solution", html.Sup("*")]),
             html.H5("Please enter a name for your solution.")
         ], 
         className="text-center"
@@ -349,10 +349,8 @@ layout = dbc.Container([
         html.Div([
             create_info_modal("training_data", "Training Data", "Please upload the training data you used to train your model. Csv and pickle (pkl) files are accepted. Please place the label to the last column of the dataframe."),
             html.Div(id="training_data_alert"),
-            html.H3("3. Training Data"),
+            html.H3(["3. Training Data", html.Sup("*")]),
             html.H5("Please upload the training data")
-
-
         ], className="text-center"),
     dcc.Upload(
         id='training_data_upload',
@@ -379,7 +377,7 @@ layout = dbc.Container([
         html.Div([
             create_info_modal("test_data", "Test Data", "Please upload the test data you used to test your model. Csv and pickle (pkl) files are accepted. Please place the label to the last column of the dataframe."),
             html.Div(id="test_data_alert"),
-            html.H3("4. Test Data"),
+            html.H3(["4. Test Data", html.Sup("*")]),
             html.H5("Please upload the test data")
             #(csv and pickle files are accepted).
             #"Please place the label to the last column of the dataframe."
@@ -405,13 +403,52 @@ layout = dbc.Container([
     ),
     html.Div(id='test_data_summary'),
     
+    # --- Y COLUMN NAME --- #
+    
+    dbc.Col([
+        html.Div([
+            create_info_modal("target_column", "Target Column", "The target column contains the values that you want to predict with your model."),
+            html.Div(id="target_column_alert"),
+            html.H3("5. Target Column"),
+            html.H5("Please select the target column")
+            #(csv and pickle files are accepted).
+            #"Please place the label to the last column of the dataframe."
+        ], className="text-center"),
+    ],
+            className="mb-4"),
+    
+    dcc.Dropdown(
+                    id='solution_set_dropdown',
+                    options=[],
+                    placeholder='Select Solution'
+    ),
+    
+    #dcc.Upload(
+    #    id='test_data_upload',
+    #    children=[
+    #        'Drag and Drop or Select a File'
+    #    ],
+    #    style={
+    #        'width': '100%',
+    #        'height': '60px',
+    #        'lineHeight': '60px',
+    #        'borderWidth': '1px',
+    #        'borderStyle': 'dashed',
+    #        'borderRadius': '5px',
+    #        'textAlign': 'center',
+    #        'margin': '10px'
+    #    }
+    #),
+    
+    html.Div(id='test_data_summary'),
+    
     # --- FACTSHEET --- #
     
     dbc.Col([
         html.Div([
             create_info_modal("factsheet", "Factsheet", "The factsheet contains the most important information about the methology used."),
             html.Div(id="factsheet_alert"),
-            html.H3("5. Factsheet"),
+            html.H3(["6. Factsheet", html.Sup("*")]),
             html.H5("Please upload the factsheet")
         ], className="text-center"),
     ],
@@ -441,7 +478,7 @@ layout = dbc.Container([
         html.Div([
             create_info_modal("model", "Model", "Please upload the model you want to assess."),
             html.Div(id="model_alert"),
-            html.H3("6. Model"),
+            html.H3(["7. Model", html.Sup("*")]),
             html.H5("Please upload the model")
         ], className="text-center")
         # 5. Please upload the model as a .sav file.
@@ -464,7 +501,7 @@ layout = dbc.Container([
     ),
     html.Div(id='model_summary'),
     html.Div(html.Span(id="upload_alert")),
-    html.Div(dbc.Button("Analyze",  id='trustscore-button', color="primary", className="mt-3"), className="text-center"),
+    html.Div(dbc.Button("Analyze",  id='upload_button', color="primary", className="mt-3"), className="text-center"),
     
 ],
 fluid=False
