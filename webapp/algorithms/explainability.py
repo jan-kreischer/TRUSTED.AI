e5a89bf7954fe9aad67c1859ee61573b4b59e2d4
JoelLeupp <34285164+JoelLeupp@users.noreply.github.com>
2021-10-12 18:56:02 +0200

    configs and new solution

--- webapp/algorithms/explainability.py
+++ webapp/algorithms/explainability.py
@@ -80,7 +80,7 @@ def model_size_score(test_data, thresholds = np.array([10,30,100,500])):
     dist_score = 5- np.digitize(test_data.shape[1]-1 , thresholds, right=True) 
          
     return result(score=int(dist_score), properties={"dep" :info('Depends on','Training Data'),
-        "n_features": info("number of features", test_data.shape[1])})
+        "n_features": info("number of features", test_data.shape[1]-1)})
 
 def feature_relevance_score(clf, train_data, target_column=None, threshold_outlier = 0.03, penalty_outlier = 0.5, thresholds = [0.05, 0.1, 0.2, 0.3]):
     
